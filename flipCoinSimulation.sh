5aea1d633195d654a4c6e8eba6fd63e5e1c664ed
kdjay517 <kdjay517@gmail.com>
2021-10-10 13:16:37 +0530

    extended the tossing upto 21 times

---
+++ flipCoinSimulation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+tailCount=0;
+headCount=0;
+
+function toss_the_coin(){
+
+	if [ $coin -eq 0 ]
+	then
+		tailCount=$(($tailCount+1));
+	else
+		headCount=$(($headCount+1));
+	fi
+}
+
+
+while [ $tailCount -lt 21 -a $headCount -lt 21 ]
+do
+	coin=$((RANDOM%2))
+	toss_the_coin $coin
+done
+
+if [ $tailCount -eq 21 ]
+then
+	echo "Tail Won"
+	echo "Head loss by $(($tailCount-$headCount)) points"
+elif [ $headCount -eq 21 ]
+then
+	echo "Head won"
+	echo "Tail loss by $(($headCount-$tailCount)) points"
+elif [ $headCount -eq $tailCount ]
+then
+	echo "Match is Tie"
+else
+	echo "No one Won"
+fi
